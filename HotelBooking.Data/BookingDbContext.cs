48c52dfd6bb33b8611418bff3d3f322974c32ee2
Nguyễn Thị Hải Yến <78371439+HaiYenNguyen12@users.noreply.github.com>
2023-02-17 15:14:34 +0700

    feat/generate_dbcontext (#16)

---
+++ HotelBooking.Data/BookingDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HotelBooking.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.Data
+{
+    public class BookingDbContext : DbContext
+    {
+        public BookingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Image> ImgUrls { get; set; }
+        public DbSet<PriceQuotation> PricesQuotation { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RoomDiscount> RoomsDiscount { get; set; }
+        public DbSet<RoomFacility> RoomsFacility { get; set; }
+        public DbSet<RoomService> RoomsService { get; set; }
+        public DbSet<Duration> RoomsDuration { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
