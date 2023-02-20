01bfd3c818ba69d5bd66b4d9fce0841381400824
ORIENTSOFTWARE\hai.le <ledaihai.160500@gmail.com>
2023-02-20 11:32:06 +0700

    feat/creat_generic_repo_unit_of_work

---
+++ HotelBooking.Data/Infrastructure/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace HotelBooking.Data.Infrastructure
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetByCondition(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "");
+        T GetByID(Guid id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(Guid id);
+    }
+}
---
+++ HotelBooking.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿namespace HotelBooking.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly BookingDbContext context;
+        private bool disposed = false;
+
+        public UnitOfWork(BookingDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+    }
+}
