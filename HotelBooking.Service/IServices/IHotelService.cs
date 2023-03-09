66c0cfd433116582f87a091f7ae7185dc3840509
HaiYenNguyen12 <nguyenthihaiyen120397@gmail.com>
2023-03-09 10:43:16 +0700

    feat/crud_services_for_admin

--- HotelBooking.Service/IServices/IHotelService.cs
+++ HotelBooking.Service/IServices/IHotelService.cs
@@ -8,7 +8,14 @@ namespace HotelBooking.Service.IServices
         Task<Guid> AddHotelAsync(HotelRequest model);
         Task<IEnumerable<HotelModel>> GetHotelByAddressTypeRoomDuration(FilterHotelRequest model);
         Task<Guid> AddRoomAsync(RoomRequest model);
+
         Task<Guid> AddExtraServiceAsync(ServiceHotelModel model);
+        Task<ServiceHotelModel> GetExtraServiceById(Guid id);
+        Task<IEnumerable<ServiceHotelModel>> GetAllExtraService();
+        Task<bool> UpdateExtraService(ServiceHotelModel model);
+        Task<bool> DeleteExtraService(Guid id);
+
+
         Task<Guid> AddFacilityAsync(FacilityModel model);
         Task<bool> AddServiceAndFacilityToRoomAsync(EquipRoomRequest model);
     }
