1fb63445216c6b0712dd5521a94f4bbbf78ce104
HaiYenNguyen12 <nguyenthihaiyen120397@gmail.com>
2023-03-20 17:05:13 +0700

    feat/create_payment

---
+++ HotelBooking.Data/DTOs/PaymentRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Data.DTOs
+{
+    public class PaymentRequest
+    {
+        public int apiid { get; set; }
+        public string appuser { get; set; }
+        public long apptime { get; set; }
+        public long  amount { get; set; }
+        public string apptransid { get; set; }
+        public string embeddata { get; set; }
+        public string item { get; set; }
+        public string  mac { get; set; }
+        public string bankcode { get; set; }
+        public string discountamount { get; set; }
+        public string checksum { get; set; }
+        public string pmcid { get; set; }
+    }
+}
